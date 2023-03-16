8f0eeed71033dec0ff8092a2d542138476ec1c3b
slavaroskoshnyy <slava.roskoshnyy@gmail.com>
2023-03-16 10:24:10 +0300

    2

--- js/script.js
+++ js/script.js
@@ -1,31 +1,41 @@
-const formNode = document.querySelectorAll('.comment')
 const clearBtnNode = document.querySelector('#comment-clear');
+const clearFirstCommentBtnNode = document.querySelector('#comment-clear-one');
+const wrapperBtnNode = document.querySelector('.js-form-btn');
 const commntFielOutNode = document.querySelector('#commen-field');
 const fieldsNode = document.querySelectorAll('.form-kontrol')
 const commentNameNode = document.querySelector('#comment-name');
 const commentBodyNode = document.querySelector('#comment-body');
-const errorMessagNode = document.querySelectorAll('.js-comment-error');
-const outMessagNode = document.querySelector('#commen-field')
+const commentDataNode = document.querySelector('#сomment-date');
+const outMessagNode = document.querySelector('#commen-field');
 
 let comments = [];
 loadComments();
 valideitionFocus(commentNameNode);
 valideitionFocus(commentBodyNode);
 
+commentBodyNode.addEventListener('input', ()=>{
+	localStorage.setItem('texst', commentBodyNode.value)
+})
+
+document.querySelector('#clear-form').onclick = ()=>{
+	localStorage.removeItem('texst');
+}
+
+commentBodyNode.value = localStorage.getItem('texst');
 
 function valideitionFocus(node){
 	node.onblur = function() {
 	if (!this.value) {
 		this.classList.add('error');
-		this.nextElementSibling.innerHTML = 'Не верный ввод'
-	}
+		this.nextElementSibling.innerHTML = 'Введите текст.'
+	};
  };
 
  node.oninput = function() {
 	if (this.classList.contains('error')) {
 		this.nextElementSibling.innerHTML = ''
 	  	this.classList.remove('error');
-	}
+	};
  };
 };
 
@@ -35,7 +45,7 @@ function valideition(){
 	fieldsNode.forEach(function(input){
 		if (input.value === ''){
 			input.classList.add('error');
-			input.nextElementSibling.innerHTML = 'Заполните поле'	
+			input.nextElementSibling.innerHTML = 'Заполните поле!'	
 		} else {
 			input.nextElementSibling.innerHTML = ''
 			input.classList.remove('error');
@@ -44,40 +54,45 @@ function valideition(){
 
 	if (emptiImputs.length !== 0){
 		return click;	
-	}
-}
+	};
+};
+
+if (commntFielOutNode.children.length != 0){
+	wrapperBtnNode.classList.remove('none')
+} ;
 
 function saveComments() {
-	localStorage.setItem('comments', JSON.stringify(comments))
+	localStorage.setItem('comments', JSON.stringify(comments));
+	
 };
 
 function loadComments() {
 	if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
 	showComments();
-}
+	
+};
 
 function showComments() {
-	const commntFielOutNode = document.querySelector('#commen-field');
 	let out = '';
 
 	comments.forEach(function(item){
-		out += `<div class="out-put">
-					<p class="text-out">${timeConverter(item.time)}</p>	
-					<p class="text-out">${item.name}</p>
-					<p class="text-out">${item.body}</p>
-					<div class="count" hidden>1</div>
-					<button class="like"></button>
-					<button class="remove-button"></button>			
+		out += ` <div class="out-put">
+						<p class="text-out">${timeConverter(item.time)}</p>	
+						<p class="text-out">${item.name}</p>
+						<p class="text-out">${item.body}</p>
+						<div class="count-like">
+							<p class="count none">1</p>
+							<button class="like"></button>
+						</div>
+						<button class="remove-button"></button>			
 					</div>`;
 	});
 	commntFielOutNode.innerHTML = out;
 };
 
- const outPut = document.querySelectorAll('.out-put')
-
 function timeConverter(UNIX_timestamp) {
 	let a = new Date(UNIX_timestamp * 1000);
-
+	
 	let diffMs = new Date() - a;
    let diffSec = Math.round(diffMs / 1000);
    let diffMin = diffSec / 60;
@@ -92,10 +107,10 @@ function timeConverter(UNIX_timestamp) {
 	let min = String(a.getMinutes()).padStart(2, '0');
 	let time = `${date} ${month} ${year} ${hour}:${min}`;
 
-	if (diffDay < 1){
+	if (diffDay < 1 && Math.sign(diffDay) != -1){
 		return `Сегодня, ${hour}:${min}`
 	}
-	if (diffDay < 2){
+	if (diffDay < 2 && Math.sign(diffDay) != -1){
 		return `Вчера, ${hour}:${min}`
 	}
 	return time;
@@ -104,115 +119,69 @@ function timeConverter(UNIX_timestamp) {
 document.getElementById('comment-add').onclick = function click(){
 	event.preventDefault();
 
-	valideition()
-
-	let xButton = document.createElement("button");
-
+	let dataValue = commentDataNode.valueAsNumber/1000 + 
+			(new Date().getUTCHours()*60*60) + 
+			(new Date().getMinutes()*60) + 
+			(new Date().getSeconds());
+	
+	valideition();
+	
 	const commet = {
 		name: commentNameNode.value,
 		body: commentBodyNode.value,
-		time: Math.floor(Date.now()/1000),
-		xButton
-	}
+		time: dataValue || Math.floor(Date.now()/1000),
+	};
 	
 	commentBodyNode.value = '';
 	commentNameNode.value = '';
+	commentDataNode.value = '';
+	localStorage.removeItem('texst');
 
 	comments.push(commet);
 	saveComments();
 	showComments();
 
+	if (commntFielOutNode.children.length != 0){
+		wrapperBtnNode.classList.remove('none');
+	};
 };
 
-// outPut.forEach(elem => {
-// 	const index = [...document.querySelectorAll('.out-put')].indexOf(elem) //! проверка индекса
-// 	console.log(index);
-// 	console.log(elem);
+outMessagNode.addEventListener("click", ({ target }) => {
+	const likeBtn = target.closest(".like");
+	
+	if (!likeBtn) return;
 
-// });
+	const count = target.closest(".count-like");
+	count.firstElementChild.classList.toggle('none');
+});
+
 
 outMessagNode.addEventListener('click', function(){
 	if (event.target.className != 'remove-button') return;
 	
 	let coment = event.target.closest('.out-put');
 	coment.remove();
-});
 
-// const counts = document.querySelectorAll('.count')
-// 	counts.forEach(elem=> {
-		
-// 		document.querySelectorAll('like').onclick = function (){
-// 			console.log(elem);
-// 			elem.hidden = !elem.hidden;
-// 		}
-// 		elem.addEventListener('click', function(){
-			
-			
-// 		})
-		
-// 	});
-
-outMessagNode.addEventListener('click', function(){
-	if (event.target.className != 'like') return;
-	
-	// let count = event.target.closest('.count');
-
-	// let count = event.target.closest('.count')
-	
-	// count.classList.toggle("none")
-
-	
-	// // const like = document.querySelectorAll('like');
-	// const count = document.createElement("div");
-	// count.dataset.counter = true
-	// count.textContent= 1;
-	// count.value = count.textContent 
-	
-	// console.log(count.value)
-	// if ((event.target.dataset.counter = true) != undefined) { // если есть атрибут...
-   //    // i++
-		
-	// 	console.log(count);
-		
-   //  }
-
-	// coment.appendChild(count)
-	// 	if (count){
-	// 		// count.innerHTML = ""
-	// 	}
-			
-	
-	
+	if (commntFielOutNode.children.length === 0){
+	wrapperBtnNode.classList.add('none');
+	} ;
 });
 
 clearBtnNode.addEventListener('click', function(){
 
-	// comments.length = 0;			
+	comments.length = 0;		
 	commntFielOutNode.innerHTML = '';
-	// localStorage.removeItem('comments');
-
+	localStorage.removeItem('comments');
+	wrapperBtnNode.classList.add('none');
 });
 
+clearFirstCommentBtnNode.addEventListener('click', function(){
 
+	commntFielOutNode.removeChild(commntFielOutNode.lastElementChild);
+	comments.splice(comments.length-1);
+	saveComments();
 
-
-
-
-// let panes = document.getElementsByClassName("pane");
-// for (let pane of panes) {
-//     let xButton = document.createElement("button"); // создаём кнопку
-//     xButton.className = "remove-button";
-//     xButton.innerHTML = "[x]";
-    
-//     pane.style.position = "relative";               // позиционируем кнопку
-//     xButton.style.position = "absolute";            // относительно сообщения
-//     xButton.style.top = "0";
-    
-//     xButton.onclick = () => pane.remove();          // функционал кнопки
-    
-//     pane.append(xButton);                           // вставляем кнопку в сообщение
-// }
-
-// node.removeChild(node.lastChild) //! удалить по одному элементу
-// node.querySelectorAll('*').forEach((n) => n.remove()); //! еще один вариант удалить все
-
+	if (commntFielOutNode.children.length === 0){
+		wrapperBtnNode.classList.add('none');
+	};
+});
